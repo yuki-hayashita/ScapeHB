ffef8791998ea0f83f48de964ed315428e0a8e40
eZm6CPtdFsfRzrqi <eZm6CPtdFsfRzrqi@gmail.com>
2022-02-05 20:07:17 -0500

    hello

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import ReactAudioPlayer from "react-audio-player";
+import Landscape from "./Landscape";
 import "./Main.css";
 
 function Main() {
@@ -8,12 +9,13 @@ function Main() {
 
     return (
       <div className="screen">
-          <video autoPlay muted onEnded={() => setEnded(true)} src="SCAPEH@B.mp4" className="video"/>
-          <ReactAudioPlayer autoPlay={true} src="IntroMusic.mp3"/>
+          {/*<video autoPlay muted onEnded={() => setEnded(true)} src="SCAPEH@B.mp4" className="video"/>*/}
+          {/*<ReactAudioPlayer autoPlay={true} src="IntroMusic.mp3"/>*/}
 
-            {ended ? <Link to="/landscape" className="link">
-                <img src="StartButton.png" className="start-img"/>
-            </Link> : null} 
+          {/*  {ended ? <Link to="/landscape" className="link">*/}
+          {/*      <img src="StartButton.png" className="start-img"/>*/}
+          {/*  </Link> : null}*/}
+          <Landscape/>
       </div>
     );
   }
